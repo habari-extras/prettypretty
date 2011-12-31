847a97c2d2eea0fbe6b64eeda056d8d52277f818
RandyWalker <USER@DOMAIN>
2011-12-31 03:03:25 +0000

    Updated for Habari 0.8.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/prettypretty/trunk@4084 f4a90942-1786-4aa1-b092-57c31935ddc3

--- prettypretty.plugin.php
+++ prettypretty.plugin.php
@@ -3,13 +3,12 @@
 class prettyPrettyAdmin extends Plugin
 {
 	/**
-	 * Removes system admin stylesheet from all admin headers, replaces it with the one from this plugin.
+	 * Adds pretty pretty stylesheet!.
 	 *
 	 */
 	public function action_admin_header( $theme )
 	{
-		Stack::remove('admin_stylesheet', 'admin');
-		Stack::add('admin_stylesheet', array($this->get_url(true) . 'admin.css', 'screen'));
+		Stack::add('admin_stylesheet', array($this->get_url(true) . 'prettypretty.css', 'screen'));
 	}
 }
 
