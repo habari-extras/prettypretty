5fc049620f973e9fc9c1b8cdc18347896b1c4c4d
mikelietz <codger+github@gmail.com>
2012-10-30 14:01:04 +0000

    Update to use the new stack names (otherwise it is too early on the page). Also add a guid.

--- prettypretty.plugin.php
+++ prettypretty.plugin.php
@@ -8,7 +8,7 @@ class prettyPrettyAdmin extends Plugin
 	 */
 	public function action_admin_header( $theme )
 	{
-		Stack::add('admin_stylesheet', array($this->get_url(true) . 'prettypretty.css', 'screen'));
+		Stack::add('admin_stylesheet', array($this->get_url(true) . 'prettypretty.css', 'screen'), 'prettypretty-css', array( 'admin-css' ));
 	}
 }
 
